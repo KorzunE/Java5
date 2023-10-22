a06837bcdf55673aded89c61f0130582031f773f
Евгений Корзун <89513152520@yandex.ru>
2023-10-22 20:02:12 +0300

    Add parametrizedTest

--- src/test/java/org/example/Java5/MoneyCalcServiceTest.java
+++ src/test/java/org/example/Java5/MoneyCalcServiceTest.java
@@ -2,34 +2,18 @@ package org.example.Java5;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class MoneyCalcServiceTest {
 
-    @Test
-        public void countCalc10000() {
-
-        MoneyCalcService service = new MoneyCalcService();
-        int income = 10000;
-        int expense = 3000;
-        int threshold = 20000;
-        int expected = 3;
-
-        int actual = service.calculate(income, expense, threshold);
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void countCalc100000() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/moneyCalc.csv")
+    public void countCalc10000(int income, int expense, int threshold, int expected) {
 
         MoneyCalcService service = new MoneyCalcService();
 
-        int income = 100000;
-        int expense = 60000;
-        int threshold = 150000;
-        int expected = 2;
-
         int actual = service.calculate(income, expense, threshold);
-
         Assertions.assertEquals(expected, actual);
     }
 }
